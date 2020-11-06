697d2ddefcea0119d3d7a35f613fc1a9db602f90
Hietzu <veeti.hietanen03@gmail.com>
2020-11-06 12:35:15 +0200

    added stuff

--- Game.js
+++ Game.js
@@ -2,6 +2,15 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var playerWidth = 50;
 var playerHeight = 50;
+var canvasWidth = 720;
+var canvasHeight = 500;
+var playerSpeed = 10;
+var playerX = (canvasWidth-playerWidth);
+var playerY = (canvasHeight-playerHeight);
+var rightPressed = false;
+var leftPressed = false;
+var upPressed = false;
+var downPressed = false;
 var time = 0
 
 function drawPlayer() {
@@ -10,14 +19,52 @@ function drawPlayer() {
     ctx.stroke()
 }
 
-function drawScore() {
-    ctx.font = "16px Arial";
-    ctx.fillStyle = "#ffffff";
-    ctx.fillText("Score: "+score, 8, 20);
+function drawTime() {
+    ctx.font = "25px Arial";
+    ctx.fillStyle = "black";
+    ctx.fillText("Time: "+time, 20, 35);
     }
 
 function draw() {
     drawPlayer();
-    ctx-clearRect(0, 0, canvas.width, canvas.height);
-
+    drawTime();
 }
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.key == "d") {
+        rightPressed = true;
+    } else if(e.key == "a") {
+        leftPressed = true;
+    } else if(e.key == "w") {
+        upPressed = true;
+    } else if(e.key == "s") {
+        downPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.key == "d") {
+        rightPressed = false;
+    } else if(e.key == "a") {
+        leftPressed = false;
+    } else if(e.key == "w") {
+        upPressed = false;
+    } else if(e.key == "s") {
+        downPressed = false;
+    }
+}
+
+if(rightPressed && playerX < canvasWidth-playerWidth) {
+    playerX += playerSpeed;
+} else if(leftPressed && playerX > 0) {
+    playerX -= playerSpeed;
+} else if(upPressed && playerY > 0) {
+    playerY -= playerSpeed;
+} else if(downPressed && playerY < canvasHeight-playerHeight) {
+    playerY += playerSpeed;
+}
+
+draw();
